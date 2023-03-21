a5182df8150d8ec27a1611d1ba61872e0209102f
wilstermanz <wilstermanz@gmail.com>
2023-03-21 14:33:12 -0500

    Fixed parent pointer of moved node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,6 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		if (parent->left)
 		{
 			leftNode->left = parent->left;
+			leftNode->left->parent = leftNode;
 		}
 		parent->left = leftNode;
 		return (leftNode);
